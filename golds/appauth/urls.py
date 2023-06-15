eb5d5ec6b36595dff79bcae1675f929eada801fc
krishna <krisnas9792@gmail.com>
2023-06-15 17:48:50 +0530

    Adding login signup page and adding authentication

--- golds/appauth/urls.py
+++ golds/appauth/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from appauth import views
 
 urlpatterns = [
-   path('',views.Home, name="Home")
-
+   path('',views.Home, name="Home"),
+   path('signup', views.signup, name="signup"),
+   path('login', views.login, name="login"),
+   path('logout', views.logout, name="logout")
 
    
 ]
